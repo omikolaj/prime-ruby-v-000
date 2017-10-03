dd8439f1b016dc6760783293724ebaceb0a11ed3
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-03 10:18:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,5 @@
 # Add  code here!
 require 'benchmark'
-def primee?(num)
-  if (num.even?) || (num <0 || num == 0 || num == 1)
-    return false
-  else
-    (2..num).each do |divisor|
-      return false if num % divisor == 0
-    end
-  end
-  return true
-  end
-end
-end
-
-
-
-
 def prime?(n)
   if(n == 1) || (n < 0) || (n.even? && n != 2)
     return false
