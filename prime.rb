b310aa05d508504f280add73a565ece49a64f566
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-03 10:19:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -16,4 +16,25 @@ def prime?(n)
   end
 end
 
+def prime?(n)
+  if(n == 1) || (n < 0) || (n.even? && n != 2)
+    return false
+  else
+    arr = (2..n).to_a
+    i = 0
+    numbers = []
+    while(i<arr.length)
+        if(n % arr[i] == 0)
+          numbers << arr[i]
+        end
+        i+=1
+    end
+    if(numbers.length > 3)
+      return false
+    else
+      return true
+    end
+  end
+end
+
 puts Benchmark.measure { prime?(1928374) }
