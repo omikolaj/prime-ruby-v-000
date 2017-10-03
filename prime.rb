22ef36a6ee45a906bd7243a59b6d7e52e94f7fae
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-03 09:59:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,26 +4,22 @@ def primee?(num)
   if (num.even?) || (num <0 || num == 0 || num == 1)
     return false
   else
-    (2...num).each do |divisor|
+    (2..num).each do |divisor|
       return false if num % divisor == 0
     end
   end
   true
 end
 
-def prime?(n)
+def primee?(n)
   if(n == 1) || (n < 0) || (n.even? && n != 2)
-
     return false
   else
     arr = (2..n).to_a
     i = 0
-    #numbers = []
     while(i<arr.length-1)
         if(n % arr[i] == 0)
           return false
-
-          #numbers << arr[i]
         end
         i+=1
     end
